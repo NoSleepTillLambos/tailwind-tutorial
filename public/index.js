371aca4fba028f2ae2df1e11a205edd39efba7c6
Liam Wedge <21100218@virtualwindow.co.za>
2024-03-02 16:36:43 +0200

    project done

---
+++ public/index.js
@@ -0,0 +1,11 @@
+// getting the burger elements in order to get the menu opening and closing
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
